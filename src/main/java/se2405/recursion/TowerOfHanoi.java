6e4a37a8025328d2b21dbe1fe1275a3a2050dc54
Sultan8707 <240254@astanait.edu.kz>
2025-09-28 18:39:25 +0500

    implement recursive Tower of Hanoi

---
+++ src/main/java/se2405/recursion/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+package se2405.recursion;
+
+/**
+ * Solves Tower of Hanoi using recursion.
+ */
+public class TowerOfHanoi {
+    public static void solve(int n, char from, char to, char aux) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + from + " to " + to);
+            return;
+        }
+        solve(n - 1, from, aux, to);
+        System.out.println("Move disk " + n + " from " + from + " to " + to);
+        solve(n - 1, aux, to, from);
+    }
+
+    // Time: O(2^n), Space: O(n)
+}
